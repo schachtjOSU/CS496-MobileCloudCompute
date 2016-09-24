0eaf068a139968d2360582eb48f053658cbdeee0
Jeffrey Schachtsick <schachtj@oregonstate.edu>
2016-09-24 16:17:13 -0700

    updates too app and main

--- HelloProject/main.py
+++ HelloProject/main.py
@@ -1,20 +1,17 @@
-from flask import Flask
+import webapp2
 import datetime         # for getting the current time
-app = Flask(__name__)
-app.config['DEBUG'] = True
 
-# Note: We don't need to call run() since our application is embedded within
-# the App Engine WSGI application server.
+class MainHandler(webapp2.RedirectHandler):
+    def getTime(self):
+        """Return the current date and time"""
+        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+        self.response.write('This is the current time: ', current_time)
 
+app = webapp2.WSGIApplication([
+    ('/', MainHandler)
+], debug=True)
 
-@app.route('/')
-def getTime():
-    """Return the current date and time"""
-    current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-    return 'The current date and time is: ', current_time
-
-
-@app.errorhandler(404)
-def page_not_found(e):
-    """Return a custom 404 error."""
-    return 'Sorry, nothing at this URL.', 404
+#@app.errorhandler(404)
+#def page_not_found(e):
+#    """Return a custom 404 error."""
+#    return 'Sorry, nothing at this URL.', 404
