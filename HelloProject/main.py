f64b14fb6dc1ccc887735ed3d3b2a30652b52e2e
Jeffrey Schachtsick <schachtj@oregonstate.edu>
2016-09-24 16:28:20 -0700

    test of main

--- HelloProject/main.py
+++ HelloProject/main.py
@@ -1,11 +1,12 @@
 import webapp2
-import datetime         # for getting the current time
+#import datetime         # for getting the current time
 
 class MainHandler(webapp2.RedirectHandler):
     def getTime(self):
         """Return the current date and time"""
-        current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
-        self.response.write('This is the current time: ', current_time)
+        #current_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M")
+        #self.response.write('This is the current time: ', current_time)
+        self.response.write('This is the current time: ')
 
 app = webapp2.WSGIApplication([
     ('/', MainHandler)
